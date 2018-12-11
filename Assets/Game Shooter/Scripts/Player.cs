72cb1ea66d1c24891fc4898b9b6ed13d3834b2c7
Rafael Victor Melo Resende <rafael.resende@outlook.com>
2018-12-10 22:02:08 -0200

    primeiro

---
+++ Assets/Game Shooter/Scripts/Player.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public bool canTripleShot = false;
+
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _fireRate = 0.25f;
+    private float _canFire = 0.0f;
+
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private GameObject _tripleShotPrefab;
+
+    
+
+	void Start () {
+        //current pos = new position
+        transform.position = new Vector3(0, 0, 0);
+
+	}
+	void Update () {
+        Movement();
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        if (Time.time > _canFire)
+        {
+            if(canTripleShot == false) { 
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.9f, 0), Quaternion.identity);
+            }else if(canTripleShot == true)
+            {
+                Instantiate(_tripleShotPrefab, transform.position, Quaternion.identity);
+            }
+            _canFire = Time.time + _fireRate;
+        }
+
+    }
+
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * _speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * _speed * verticalInput * Time.deltaTime);
+
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        if (transform.position.x > 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0);
+        }
+    }
+    public void TripleShotPowerupOn() {
+        canTripleShot = true;
+        StartCoroutine(TripleShotPowerDownRoutine());
+
+    }
+    public IEnumerator TripleShotPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+
+        canTripleShot = false;
+    }
+}
